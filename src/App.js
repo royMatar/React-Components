c6ed257f06d508270a33b08bc3d1ca94540e7021
Roy <roy.mattar@outlook.com>
2023-06-08 03:42:59 +0300

    Calculator app and React-player components added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import PassingProps from "./Components/PassingProps";
 import Fruits from "./Components/ManagingState/Fruits";
 import FruitsCounter from "./Components/ManagingState/FruitsCounter";
 import { useState } from "react";
-
-
+import YoutubeVideo from './Components/React-player/YoutubeVideo'
+import Songselection from './Components/SongSelection/SongSelection';
+import Calculator from "./Components/Calculator/Calculator";
 
 function App() {
   const [fruits] = useState([
@@ -14,6 +15,10 @@ function App() {
     { fruitName: "peach", id: 2 },
     { fruitName: "apple", id: 3 },
     { fruitName: "mango", id: 4 },
+    { fruitName: "mango", id: 5 },
+    { fruitName: "mango", id: 6 },
+    { fruitName: "Olive tree", id: 7 },
+    { fruitName: "lemon", id: 8 },
   ]);
   
   return (
@@ -23,6 +28,9 @@ function App() {
       <h1>Fruits:</h1>
       <Fruits fruits={fruits} />
       <FruitsCounter fruits={fruits} />
+      <YoutubeVideo/>
+      <Songselection/>
+      <Calculator/>
     </div>
   );
 }
---
+++ src/Components/React-player/YoutubeVideo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube";
+
+function YoutubeVideo() {
+  return (
+    <div>
+      <ReactPlayer url="https://www.youtube.com/watch?v=VS-GZYv16qM" />
+    </div>
+  );
+}
+
+export default YoutubeVideo;
